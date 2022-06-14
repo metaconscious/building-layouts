ed436fc6515ef045312f9042e342624a83042ed1
Marko <94597129+metaconscious@users.noreply.github.com>
2022-06-14 15:38:35 +0800

    implement the title row

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,53 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Flutter layout demo'),
           ),
-          body: const Center(
-            child: Text('Hello World'),
+          body: Column(
+            children: const [
+              TitleSection(),
+            ],
+          )
+        ));
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
           ),
-        )
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41')
+        ],
+      ),
     );
   }
 }
